36a33625eca846a41863b5c9a9db1efa76016e04
Mehmet Öztürk <mehmetozturk@Mehmet-MacBook-Pro.local>
2021-10-21 20:40:12 +0300

    MERN Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <MainNavigation/>
+          <main>
+            <Switch>
+              <Route path="/" exact>
+                <Users />
+              </Route>
+              <Route path="/places/new" exact>
+                <NewPlace />
+              </Route>
+              <Redirect to="/" />
+            </Switch>
+          </main>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/places/pages/NewPlaces.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const NewPlace=()=>{
+    return (
+        <h1>New Places</h1>
+    )
+}
+export default NewPlace
